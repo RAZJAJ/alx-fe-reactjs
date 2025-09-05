a72cc5a010dc8d2ad2581c837b48ae36c0421cbe
RAZJAJ <109910534+RAZJAJ@users.noreply.github.com>
2025-09-05 01:39:36 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -22,6 +23,15 @@ function App() {
       <Footer />
     </div>
   );
+
+<div>
+      <h1>Welcome to the User Profile App</h1>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </div>
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
