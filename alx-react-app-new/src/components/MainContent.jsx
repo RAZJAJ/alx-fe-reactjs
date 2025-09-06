d9a2f6f5654b47aa0951928942c096fed2f86bbc
RAZJAJ <109910534+RAZJAJ@users.noreply.github.com>
2025-09-07 00:01:28 +0100

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,9 +3,7 @@ function MainContent() {
     <main style={{ backgroundColor: '#f4f4f4', padding: '20px', minHeight: '200px' }}>
       <h2 style={{ textAlign: 'center', color: 'darkred' }}>Welcome to My Page</h2>
       <p style={{ textAlign: 'justify', lineHeight: '1.6' }}>
-        I love to visit <span style={{ fontWeight: 'bold', color: 'navy' }}>New York</span>,{" "}
-        <span style={{ fontWeight: 'bold', color: 'purple' }}>Paris</span>, and{" "}
-        <span style={{ fontWeight: 'bold', color: 'darkgreen' }}>Tokyo</span>.
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
   );
