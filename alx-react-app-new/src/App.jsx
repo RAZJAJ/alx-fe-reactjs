1c296b4d95402d1905e9ca1277e282fed011f49f
RAZJAJ <109910534+RAZJAJ@users.noreply.github.com>
2025-09-07 00:06:05 +0100

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import Counter from './components/Counter';  
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <UserProfile name="AbdulRazaq" age={24} bio="Systems Engineer and React learner" />
+      <Counter /> 
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
 
 function App() {
   const [count, setCount] = useState(0)
