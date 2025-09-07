5bc0bc1e2164a1718ce47f802e00074e9ce62258
RAZJAJ <109910534+RAZJAJ@users.noreply.github.com>
2025-09-07 22:54:42 +0100

    Update Footer.jsx

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ background: "#333", color: "#fff", textAlign: "center", padding: "10px", marginTop: "20px" }}>
+      <p>&copy; {new Date().getFullYear()} My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
