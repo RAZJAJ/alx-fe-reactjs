0a4a1e3449d65c16326e0d957ef6fa896a5a54c0
RAZJAJ <109910534+RAZJAJ@users.noreply.github.com>
2025-09-14 00:54:24 +0100

    Create Home.jsx

---
+++ my-company/src/components/Home.jsx
@@ -0,0 +1,13 @@
+
+function Home() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1 style={{ color: '#333' }}>Welcome to Our Company</h1>
+      <p style={{ color: '#555' }}>
+        We are dedicated to delivering excellence in all our services.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
