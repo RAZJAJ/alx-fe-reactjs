99f07bb3528cf30bd7a148f46e6a7fce10c0d841
RAZJAJ <109910534+RAZJAJ@users.noreply.github.com>
2025-09-14 00:55:59 +0100

    Create Services.jsx

---
+++ my-company/src/components/Services.jsx
@@ -0,0 +1,15 @@
+
+function Services() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1 style={{ color: '#333' }}>Our Services</h1>
+      <ul style={{ listStyleType: 'disc', color: '#555' }}>
+        <li style={{ marginBottom: '10px' }}>Technology Consulting</li>
+        <li style={{ marginBottom: '10px' }}>Market Analysis</li>
+        <li style={{ marginBottom: '10px' }}>Product Development</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
